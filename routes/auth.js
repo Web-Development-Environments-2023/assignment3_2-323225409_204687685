dc95549baee70813a29c9d8a44813dde3dc5bcbf
ayemoy <ayelet431@gmail.com>
2023-05-27 12:49:29 +0300

    ok

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 var express = require("express");
+const app = express();
+const port = process.env.PORT || "3000";
+app.use(express.json());
 var router = express.Router();
+
 const MySql = require("./utils/MySql");
 const DButils = require("./utils/DButils");
 const bcrypt = require("bcrypt");
@@ -10,13 +14,14 @@ router.post("/Register", async (req, res, next) => {
     // valid parameters
     // username exists
     let user_details = {
+      // userID: req.body.userID,
       username: req.body.username,
       firstname: req.body.firstname,
       lastname: req.body.lastname,
       country: req.body.country,
       password: req.body.password,
       email: req.body.email,
-      profilePic: req.body.profilePic
+      // profilePic: req.body.profilePic
     }
     let users = [];
     users = await DButils.execQuery("SELECT username from users");
@@ -30,9 +35,13 @@ router.post("/Register", async (req, res, next) => {
     //   parseInt(process.env.bcrypt_saltRounds)
     // );
     await DButils.execQuery(
-      `INSERT INTO users VALUES ('${userID}','${user_details.username}', '${user_details.firstname}', '${user_details.lastname}',
+      `INSERT INTO users (username,firstname,lastname,country,password,email) VALUES ('${user_details.username}', '${user_details.firstname}', '${user_details.lastname}',
       '${user_details.country}', '${user_details.password}', '${user_details.email}')`
     );
+    // await DButils.execQuery( original
+    //   `INSERT INTO users VALUES ('${user_details.username}', '${user_details.firstname}', '${user_details.lastname}',
+    //   '${user_details.country}', '${user_details.password}', '${user_details.email}', '${user_details.profilePic}')`
+    // );
     res.status(201).send({ message: "user created", success: true });
   } catch (error) {
     next(error);
