5f2ea1308426ad1e76ea0a3f23eeedc49a138942
ayemoy <ayelet431@gmail.com>
2023-05-28 16:37:15 +0300

    commited just for u honey

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,25 @@
 var express = require("express");
+const app = express();
+const morgan = require("morgan"); // logging library
+const port = process.env.PORT || "3000";
+app.use(express.json()); // parse application/json
 var router = express.Router();
-const DButils = require("./utils/DButils");
+const MySql = require("../routes/utils/MySql");
+const DButils = require("../routes/utils/DButils");
 const user_utils = require("./utils/user_utils");
 const recipe_utils = require("./utils/recipes_utils");
-const app = express();
-const port = process.env.PORT || "3000";
-app.use(express.json()); 
+
+router.get("/", (req, res) => res.send("im in user"));
 /**
  * Authenticate all incoming requests by middleware
  */
 router.use(async function (req, res, next) {
+  console.log("$$$$$$$$$$$")
+  console.log(req.session)
+  console.log(req.session.user_id)
+  console.log("############")
   if (req.session && req.session.user_id) {
-    DButils.execQuery("SELECT user_id FROM users").then((users) => {
+    DButils.execQuery("SELECT userID FROM users").then((users) => {
       if (users.find((x) => x.user_id === req.session.user_id)) {
         req.user_id = req.session.user_id;
         next();
